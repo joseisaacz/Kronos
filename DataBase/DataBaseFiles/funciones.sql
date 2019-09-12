d5b3d0c9c821ff90efa42836fc9b58bb4885f8d6
joseisaacz <39786721+joseisaacz@users.noreply.github.com>
2019-09-11 20:36:36 -0600

    holly shit

---
+++ DataBase/DataBaseFiles/funciones.sql
@@ -0,0 +1,13 @@
+
+CREATE PROCEDURE insertAccord(
+IN accNumber VARCHAR(100), IN incorDate DATE, 
+IN deadLine DATE, IN sessionDate DATE, IN types
+VARCHAR(100), IN observations longtext, IN publics TINYINT(4),
+IN notified TINYINT(4), IN states CHAR(1), IN office VARCHAR(45) 
+)
+BEGIN
+INSERT INTO T_ACCORD (ACCNUMBER, INCORDATE, 
+DEADLINE, SESSIONDATE, TYPE, OBSERVATIONS, PUBLIC, NOTIFIED,  STATE,
+OFFICE) VALUES (accNumber, incorDate, deadLine, sessionDate, types, observations, publics,
+notified, states, office);
+END;
